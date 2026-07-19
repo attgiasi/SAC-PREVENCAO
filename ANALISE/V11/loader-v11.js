b0449b1c199eb89d2f9eaba713d7ee5f17601d0c
attgiasi <attgiasi@users.noreply.github.com>
2026-07-18 23:57:37 -0300

    chore(v11): fixar loader 11.21

--- ANALISE/V11/loader-v11.js
+++ ANALISE/V11/loader-v11.js
@@ -4,8 +4,8 @@
   const REPOSITORY = "attgiasi/SAC-PREVENCAO";
   const BRANCH = "main";
   const BUILD_PATH = "ANALISE/V11";
-  const LOADER_VERSION = "11.20.0";
-  const SAFE_FALLBACK_REF = "5f390d3b1000f841f9ed58f655948a3a41292905";
+  const LOADER_VERSION = "11.21.0";
+  const SAFE_FALLBACK_REF = "1580d4adb11218f1dbb5d2ec50cc8838548559bb";
   const RELEASE_MANIFEST = `https://raw.githubusercontent.com/${REPOSITORY}/${BRANCH}/${BUILD_PATH}/release-v11.json`;
   const FILES = Object.freeze([
     "sac-memory-v11.js",

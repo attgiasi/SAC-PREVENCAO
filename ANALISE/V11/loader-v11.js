b6e4f6bd93fe0154a06454d823ddf23229e4f0dc
attgiasi <attgiasi@users.noreply.github.com>
2026-07-21 12:44:48 -0300

    chore: apontar loader V11.23 para build imutável

--- ANALISE/V11/loader-v11.js
+++ ANALISE/V11/loader-v11.js
@@ -4,8 +4,8 @@
   const REPOSITORY = "attgiasi/SAC-PREVENCAO";
   const BRANCH = "main";
   const BUILD_PATH = "ANALISE/V11";
-  const LOADER_VERSION = "11.22.0";
-  const SAFE_FALLBACK_REF = "52c236e8fd07fae9da8f24b881706eae34cf773f";
+  const LOADER_VERSION = "11.23.0";
+  const SAFE_FALLBACK_REF = "8f8dfb3cf487bd21a808d49cf9044edac5b701dd";
   const RELEASE_MANIFEST = `https://raw.githubusercontent.com/${REPOSITORY}/${BRANCH}/${BUILD_PATH}/release-v11.json`;
   const FILES = Object.freeze([
     "sac-memory-v11.js",

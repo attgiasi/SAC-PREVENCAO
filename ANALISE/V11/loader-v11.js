1c54cdffb4669c78b70bf4ef3d059fec0ce7c9ae
attgiasi <attgiasi@users.noreply.github.com>
2026-07-21 23:38:11 -0300

    chore(v11): point loader to 11.25 runtime

--- ANALISE/V11/loader-v11.js
+++ ANALISE/V11/loader-v11.js
@@ -4,8 +4,8 @@
   const REPOSITORY = "attgiasi/SAC-PREVENCAO";
   const BRANCH = "main";
   const BUILD_PATH = "ANALISE/V11";
-  const LOADER_VERSION = "11.24.0";
-  const SAFE_FALLBACK_REF = "1672700de4276d63e9648b67fb70a9ff605379eb";
+  const LOADER_VERSION = "11.25.0";
+  const SAFE_FALLBACK_REF = "bb11764eea6b0ab6c0503de48c4078cec8e1e38e";
   const RELEASE_MANIFEST = `https://raw.githubusercontent.com/${REPOSITORY}/${BRANCH}/${BUILD_PATH}/release-v11.json`;
   const FILES = Object.freeze([
     "sac-memory-v11.js",
